88353b5d77e8f2e6e6ab82960403db80550ebb32
vas-lag <63235537+vas-lag@users.noreply.github.com>
2020-08-28 17:44:45 +0300

    bcrypt

--- controller/log-in-controller.js
+++ controller/log-in-controller.js
@@ -1,5 +1,5 @@
 'use strict';
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt-nodejs');
 
 /** Διαλέξτε το κατάλληλο μοντέλο */
 const model = require('../model/tennis-club-model-mysql.js');
